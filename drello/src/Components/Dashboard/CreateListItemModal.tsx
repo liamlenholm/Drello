fd65072de28568948114db9348a2f1237cb6474a
Liam Lenholm <lenholmliam@gmail.com>
2022-12-29 20:29:15 +0100

    Fixing for New Task Modal

--- drello/src/Components/Dashboard/CreateListItemModal.tsx
+++ drello/src/Components/Dashboard/CreateListItemModal.tsx
@@ -21,21 +21,32 @@ export default function CreateListItemModal(props: ModalVisable) {
       <Modal.Body>
         <div className="space-y-6 px-6 pb-4 sm:pb-6 lg:px-8 xl:pb-8">
           <h3 className="text-xl font-medium text-gray-900 dark:text-white">
-            New Board
+            New Task
           </h3>
           <div>
             <div className="mb-2 block">
-              <Label htmlFor="boardName" value="Board Name:" />
+              <Label htmlFor="taskName" value="Task Name:" />
             </div>
             <TextInput
-              name="boardName"
-              id="boardName"
-              placeholder="Board 1"
+              name="taskName"
+              id="taskName"
+              placeholder="Task 1"
+              required={true}
+            />
+          </div>
+          <div>
+            <div className="mb-2 block">
+              <Label htmlFor="taskDescription" value="Description:" />
+            </div>
+            <TextInput
+              name="taskDescription"
+              id="taskDescription"
+              placeholder="Finnish Drello"
               required={true}
             />
           </div>
           <div className="w-full">
-            <Button onClick={() => createBoard()}>Create board</Button>
+            <Button onClick={() => createBoard()}>Create Task</Button>
           </div>
         </div>
       </Modal.Body>
